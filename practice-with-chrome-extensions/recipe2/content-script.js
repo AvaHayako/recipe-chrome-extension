cc40c4458f653cbf30764dc96071576c0385cba7
adavid2021 <sleek12o12@gmail.com>
2021-05-05 16:59:42 -0400

    progress on content-script.js

--- practice-with-chrome-extensions/recipe2/content-script.js
+++ practice-with-chrome-extensions/recipe2/content-script.js
@@ -48,23 +48,35 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 // adjust fractions does not work when doubling 5 / 2 (what?)
 
 // cannot halve integer values (huh?)
+
+// "1 1/4" -> "5/2/4" (double)
+
+// NOTES:
+// now there is an issue with fractions converting to whole numbers (calculation)
+// "5/2" -> "5"
 function reProportion(multiplier) {
     $.each($('.o-Ingredients__a-Ingredient--CheckboxLabel'), function () {
 
         let context = $(this).context.innerHTML;
 
-        var mixedFractionRegex = new RegExp(/\d+\s+\d[/]\d/g);
-        var fractionRegex = new RegExp(/(?<!\d\s)\d[/]\d/g);
-        var wholeNumberRegex = new RegExp(/(\b[^/]\d[^/]\b)|(\b(?<!(\d\s|[/]))\d(?!(\s\d|[/]))\b)/g);
+        // alternative implementation
+        // filter for EVERYTHING LOL
+        // appleRegexAndMultiply as usual
+
+
+        // var mixedFractionRegex = new RegExp(/\d+\s+\d[/]\d/g);
+        // var fractionRegex = new RegExp(/(?<!\d\s)\d[/]\d/g);
+        var allNumsRegex = new RegExp(/(\d+\s+\d[/]\d)|((?<!\d\s)\d[/]\d)|((\b[^/]\d[^/]\b)|(\b(?<!(\d\s|[/]))\d(?!(\s\d|[/]))\b))/g);
 
         // update the newContext string each time we call applyRegexAndMultiply
 
-        var newContext = applyRegexAndMultiply(context, wholeNumberRegex, multiplier);
-        newContext = applyRegexAndMultiply(newContext, mixedFractionRegex, multiplier);
-        newContext = applyRegexAndMultiply(newContext,fractionRegex, multiplier);
+        // var newContext = applyRegexAndMultiply(context, wholeNumberRegex, multiplier);
+        // newContext = applyRegexAndMultiply(newContext, mixedFractionRegex, multiplier);
+        // newContext = applyRegexAndMultiply(newContext,fractionRegex, multiplier);
+        var newContext = applyRegexAndMultiply(context, allNumsRegex, multiplier);
 
-        $(this).context.innerHTML = newContext;
-
+        $(this).context.innerHTML = "###"+newContext;
+        // $(this).context.css("color","orange");
         // I (Aidan) ADDED PLUS SIGNS ON THE OUTSIDE OF THE CONTEXT TO TRY TO DEBUG (but this will be commented)
         // $(this).context.innerHTML = "+" + newContext + "+";
     });
@@ -77,10 +89,15 @@ var gcd = function(a, b) {
 };
 
 function simplify(fract, m) {
-    const nume = fract.split("/")[0] * m * 100;
-    const denom = fract.split("/")[1] * 100;
+    const nume = fract.split("/")[0] * m * 1000;
+    const denom = fract.split("/")[1] * 1000;
+
+    // console.log("new numerator: " + nume);
+    // console.log("new numerator: " + denom);
 
     if (nume % denom === 0) { // when there is no fraction after multiplication
+        console.log("new IN SPECIAL CASE: ",(nume / denom).toString());
+
         return (nume / denom).toString()
     } else { // when there is a fraction
         const myGCD = gcd(nume, denom);
@@ -97,20 +114,24 @@ function decimal2fraction(dec) {
 
 // multiplier can be 0.25, 0.5, 2, 3, 4
 function changeValue(input, mult) {
+    // console.log("initial value is: " + input);
     let out = "";
 
     const splitInput = input.split(" ");
 
     if (splitInput.length === 1) { // HANDLE NORMAL CASE
         out = simplify(splitInput[0], mult);
+        // console.log("new NORMAL FRACTION: ",out);
     } else { // HANDLE MIX FRACTION CASE
         const whole = parseInt(splitInput[0]);
         const numerator = parseInt(splitInput[1].split('/')[0]);
         const denomonator = parseInt(splitInput[1].split('/')[1]);
         const unsimplified = (whole * denomonator + numerator).toString() + "/" + denomonator.toString();
         out = simplify(unsimplified, mult);
+        // console.log("new MIXED FRACTION: ",out);
     }
 
+    // console.log("new final fraction: " + out);
     return (out);
 }
 
@@ -136,13 +157,18 @@ function applyRegexAndMultiply(context, myRegex, multiplier) {
 
         var newValue;
         if (value.includes("/")) {
-            console.log("FRACTION FOUND: " + value);
+            // console.log("FRACTION FOUND: " + value);
             newValue = changeValue(value,multiplier);
-        }
-        else {
+            console.log("new newValue is: " + newValue);
+        } else {
+            // if value includes extra space(s) (due to regex filtering)
+            // -> remove spaces and update indexes
             if (value.includes(" ")){
                 value = Number(value.toString().trim());
                 match.index++;
+                if (value.endsWith(" ")){
+                    myRegex.lastIndex--;
+                }
             }
             else{
                 value = Number(value);
@@ -157,27 +183,38 @@ function applyRegexAndMultiply(context, myRegex, multiplier) {
         // ANOTHER PROBLEM:
         // preceding conversions in an ingredient string (e.g. in string "5 tb and 3 tsp", 5 tb will be converted before 3 tsp)
         // may affect following conversions because the index of the string will be changed.
+        // "10 tb and 3 tsp"
         // we try to account for that here.
         let shift = charArray.length - ogLength;
         let startIX = match.index + shift;
         let endIX = myRegex.lastIndex + shift;
 
-        // I learned that sometimes we need to "offset" the charArray to make room for a bigger number
-        // i.e. if our value was 5 and our newValue is 10, then we need to account for 1 new char ("0")
-        // so, we set an offset value and pop as much room in as we need
-        // DOESN'T ACCOUNT FOR 10 -> 5 :/
-        let offset = newValue.length - value.toString().length+1;
+        // I learned that sometimes we need to "offset" the charArray to make room for shorter/longer numbers
+
+        let offset = newValue.length - value.toString().length;
+        console.log("new: ",charArray.join(""),"\nshift: ",shift,"\noffset: ",offset,"\nstartIX: ",startIX,"\nendIX: ",endIX);
         if (offset > 0) {
             charArray = (charArray.join("").substring(0, startIX + 1)
                 + " ".repeat(offset)
                 + charArray.join("").substring(endIX, charArray.length)).split("");
         }
+        else if (offset < 0) {
+            charArray = (charArray.join("").substring(0, startIX+1-offset)
+                +" ".repeat(Math.abs(offset))
+                + charArray.join("").substring(endIX-1-offset, charArray.length)).split("");
+        }
 
         // for each char of the newValue (e.g. "10"),
         // update the right element in the charArray accordingly
+
+        // ONLY WORKS FOR FRACTIONS, NOT WHOLE NUMBERS >:(
+        console.log("old value: ",value,"\nnew value: ",newValue);
+        if (offset<0){
+            newValue= newValue + " ".repeat(Math.abs(offset));
+        }
         for (let i = 0; i < newValue.length; i++) {
             charArray[startIX + i] = newValue[i];
-            // console.log("new charArray",charArray);
+            console.log("new",charArray[startIX+i],"->",newValue[i]);
         }
     }
 
@@ -226,7 +263,7 @@ function makeChanges(newIngredientList) {
     finalIngredients = {"chicken": "1"}
 
     $.each($('.ingredient-amount'), function () {
-        console.log($(this).context.innerHTML);
+        // console.log($(this).context.innerHTML); // useful for debugging spacing????????????????
         $(this).context.innerHTML = "test";
 
 
