55f6c53132a46550b6b9b8439663d2a219e2ce53
Avahayako <ahbartolome5@gmail.com>
2021-05-24 21:03:17 -0400

    merged FE and BE into recipe folder

--- practice-with-chrome-extensions/recipe/content-script.js
+++ practice-with-chrome-extensions/recipe/content-script.js
@@ -1,188 +1,332 @@
-console.log('content-script.js')
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
+    console.log("DEBUG message from CS: " + message);
 
-/*
-    START OF:
-    Making the ingredient list from the webpage content (for delish.com)
+    if (hasNumber(message.toString())) {
+        reProportion(Number(message));
+    }
 
- */
-
-
-
-// We know that on delish.com, the ingredients are stored in elements of class "ingredient-item", so:
-let htmlIngredientList = Array.from(document.getElementsByClassName("ingredient-item"));
-
-// For each ingredient, store it's name and measurement
-let ingredientList = {};
-htmlIngredientList.forEach(function (html) {
-    // parse ingredient html into list where:
-    //  ["measurement","","name"]
-    let ingredientInfo = html.innerText.trim().split("\n");
-    // access right info
-    let name = ingredientInfo[2];
-    let amt = ingredientInfo[0];
-    // store that info
-    ingredientList[name] = amt;
+    if (message.toString() === "metric") {
+        convertMetrics(message);
+    }
 });
 
-console.log(ingredientList);
+// goes through each ingredient item and changes its HTML contents based on desired portion
+function reProportion(multiplier) {
+    $.each($('.o-Ingredients__a-Ingredient--CheckboxLabel'), function () {
+        let context = $(this).context.innerHTML;
+        var allNumsRegex = new RegExp(/(\d+\s+\d[/]\d)|((?<!\d\s)\d[/]\d)|((\b[^/]\d[^/]\b)|(\b(?<!(\d\s|[/]))\d(?!(\s\d|[/]))\b))/g);
+        var newContext = applyRegexAndMultiply(context, allNumsRegex, multiplier);
 
+        $(this).context.innerHTML = newContext;
+    });
+}
 
-// console.log(htmlIngredientList);
+// finds greatest common denominator between two numbers
+var gcd = function (a, b) {
+    if (!b) return a;
 
-/*
-    END OF:
-    Making the ingredient list from the webpage content (for delish.com)
- */
+    return gcd(b, a % b);
+};
 
+// converts fractions to floats
+function simplify(fract, m) {
+    const nume = fract.split("/")[0] * m * 1000;
+    const denom = fract.split("/")[1] * 1000;
 
+    if (nume % denom === 0) { // when there is no fraction after multiplication
+        console.log("new IN SPECIAL CASE: ", (nume / denom).toString());
 
+        return (nume / denom).toString()
+    } else { // when there is a fraction
+        if (nume>denom){
+            let newFract = fractionToMixedFraction(nume,denom,true);
+            let a = newFract[0];
+            let b = newFract[1];
+            let c = newFract[2];
+            const myGCD = gcd(b,c);
+            return a.toString() + " " + (b/myGCD).toString() + "/" + (c/myGCD).toString();
+        }
+        const myGCD = gcd(nume, denom);
+        return (nume / myGCD).toString() + "/" + (denom / myGCD).toString();
+    }
+}
 
+// converts floats to fractions
+function decimal2fraction(dec) {
+    const nume = dec * 100;
+    const denom = 100;
+    const myGCD = gcd(nume, denom);
+    let out = (nume / myGCD).toString() + "/" + (denom / myGCD).toString();
+    if (nume > denom) {
+        out = fractionToMixedFraction(nume,denom);
+    }
 
-/*
-    START OF:
-    Modifying the ingredient list
+    return out;
+}
 
-    This content-script will wait until the popup sends it a message about what aspects of the ingredients to modify.
-    It will then handle the message and execute a script accordingly.
-*/
+function fractionToMixedFraction(nume,denom,returnNums=false){
+    let whole = Math.floor(nume/denom);
+    if (returnNums){
+        return [whole, (nume%denom),denom];
+    }
+    let newNume = nume%denom;
+    const myGCD = gcd(newNume,denom);
+    return whole.toString() + " " + (newNume/myGCD).toString() + "/" + (denom/myGCD).toString();
+}
 
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    // Handle message.
+// determines if a number is a fraction, mixed fraction, whole number and sends it to simplify
+function changeValue(input, mult) {
+    let out = "";
 
-    console.log(message);
+    const splitInput = input.split(" ");
 
-    reProportion(Number(message));
+    if (splitInput.length === 1) { // HANDLE NORMAL CASE
+        out = simplify(splitInput[0], mult);
+    } else { // HANDLE MIX FRACTION CASE
+        const whole = parseInt(splitInput[0]);
+        const numerator = parseInt(splitInput[1].split('/')[0]);
+        const denomonator = parseInt(splitInput[1].split('/')[1]);
+        const unsimplified = (whole * denomonator + numerator).toString() + "/" + denomonator.toString();
+        out = simplify(unsimplified, mult);
+    }
+    return (out);
+}
 
+/* Applies regex function to the context (innerHTML) of some element on a FoodNetwork recipe.
+    The goal is to multiply ingredients by some value. */
+function applyRegexAndMultiply(context, myRegex, multiplier) {
+    // split up context into char array, so it's easier to change values of
+    let charArray = context.split("");
+    let ogLength = charArray.length;
 
+    /*    while there are matches in the context for our regex expression
+        (e.g. the string "5 tablespoons and 1 teaspoon" has 2 matches for wholeNumberRegex)*/
+    while ((match = myRegex.exec(context)) !== null) {
+        var value = match[0]; // e.g. "5"
+        var newValue;
+        var endsWSpace = false;
+        console.log("DEBUG dealing with "+value);
+        if (value.includes("-") || value.includes(";")){
+            if (value.startsWith("-") || value.startsWith(";")){
+                match.index++;
+            }
+            else{
+                myRegex.index--;
+            }
+            value = value.replaceAll("-","").replaceAll(";","");
+        }
+        if (value.startsWith(" ")) {
+            match.index++;
+            value = value.toString().slice(1,value.length);
+        }
+        else if(value.endsWith(" ")){
+            myRegex.lastIndex--;
+            endsWSpace = true;
+            value = value.toString().slice(0,value.length-1);
+        }
 
-    // First, figure out what the message is saying:
-    // try to parse message as int or float
-        // if it works, then we'll be modifying the ingredient portions
-        // call the reproportion function
-    // else, we're either:
-        // going to be doing metric conversion
-        // or doing substitution
-
-    // After determining what we're doing, call the related function
-
-});
-
-
-// Re-proportion all ingredients by some multiplier
-
-// WHERE WE LEFT OFF 4/15
-    // changed structure of the method:
-        // NOT looking at ingredientList
-        // instead:
-        // for each ingredient amount, isolate number and multiply it
-    // what's left:
-        // configure fraction stuff
-
-
-function reProportion(multiplier){
-
-    $.each($('.ingredient-amount'), function (){
-
-        // console.log($(this).context.innerHTML);
-        // $(this).context.innerHTML = "mommy's";
-
-        amt = $(this).context.innerHTML;
-
-        var nonLettersRegex = /[^a-zA-Z^.]/g;
-        var lettersRegex = /[a-zA-Z]/g;
-        // var wordAmt = amt.match(lettersRegex).join("");
-        var numberAmt = amt.match(nonLettersRegex).join("").trim();
-        // console.log(numberAmt);
-
-        if(numberAmt.includes("/")){
-
-            // function fractionToDecimal(fraction) {
-            //     return fraction
-            //         .split('/')
-            //         .reduce((numerator, denominator, i) =>
-            //             numerator / (i ? denominator : 1)
-            //         );
-            // }
-
-            console.log('Fraction: ',numberAmt);
-            true;
-
+        if (value.includes("/")) {
+            newValue = changeValue(value, multiplier);
         }
         else {
-            let newNum = Number(numberAmt)*multiplier;
-            console.log('Not a fraction: ', numberAmt,newNum);
-
-            $(this).context.innerHTML = newNum;
-
+            value = Number(value);
+            // multiply value
+            newValue = (value * multiplier).toString();
+            if (newValue.includes(".")) {
+                newValue = decimal2fraction(newValue);
+            }
         }
 
+        let shift = charArray.length - ogLength;
+        let offset = newValue.length - value.toString().length;
 
-    });
+        if (match.index >= 0){ // added >=0 and it did nothing
+            if(endsWSpace){
+                shift++;
+            }
+        }
 
+        let startIX = match.index + shift;
+        let endIX = myRegex.lastIndex + shift;
 
+        let front =  charArray.join("").substring(0, startIX+1);
 
-    // do stuff and update webpage
+        let middle = " ".repeat(value.length);
+        if ((Math.abs(offset)+shift)>0){
+            middle += " ".repeat(Math.abs(offset)+shift+1);
+        }
+
+        let back = charArray.join("").substring(endIX, charArray.length);
+
+        let print = ("DEBUG:\n\n"
+                    + "size of shift: " + shift + "\n"
+                    + "size of offset: " + offset + "\n\n"
+                    + "start "+startIX+", end "+endIX+'\n\n'
+                    + front.replaceAll(" ","#") + "\n\n"
+                    + middle.replaceAll(" ","#") +"\n\n"
+                    + "(what will be) new middle: " + newValue + "\n\n (no longer "+value+")\n\n"
+                    + back.replaceAll(" ","#"));
+        console.log(print);
+
+        charArray = (front + middle + back).split("");
+
+        for (let i = 0; i < newValue.length; i++) {
+            charArray[startIX + i] = newValue[i];
+        }
+    }
+
+    // finally, return the new context, i.e. the charArray turned into a string
+    return charArray.join("");
 }
 
 // Convert metrics of all ingredients
-function convertMetrics(isImperial){
-    let newIngredientList = [];
+function convertMetrics(measurementType) {
+    // if desired measurement system is metric, need to convert
+    if (measurementType === "metric") {
+        let prunedContexts = [];
+        $.each($('.o-Ingredients__a-Ingredient--CheckboxLabel'), function () {
+            const context = $(this).context.innerHTML;
+            if (!context.includes("Deselect")) {
+                prunedContexts.push(context.split(" "));
+            }
+        });
+        // pruned is a list of lists of words in each ingredient line
+        prunedContexts.forEach(function (contextArray, idx) {
+            // console.log("converting: ",contextArray);
+            prunedContexts[idx] = convertContext(contextArray, measurementType);
+        });
+        // console.log("prunedContexts: ", prunedContexts);
+
+        $.each($('.o-Ingredients__a-Ingredient--CheckboxLabel'), function (index) {
+            const context = $(this).context.innerHTML;
+
+            if (!context.includes("Deselect")) {
+                $(this).context.innerHTML = prunedContexts[index - 1].join(" ");
+            }
+        });
+    }
     // do stuff and update webpage
 }
 
-// Make substitutions to specific ingredients
-function makeSubstitutions(ingredients){
-    let newIngredientList = [];
-    // do stuff and update webpage
+// array parameter is the list of all words in one ingredient line
+function convertContext(array, measurementType) {
+    let convertedOutput = array;
+    let convertMe = 0;
+    let numberIndex = 0;
+    let size = 0;
+
+    // determining if there is even a number to convert for this ingredient line
+    let convertible = hasNumber(array.join(""));
+
+    // if we would like to convert from U.S. to metric, we go here
+    if (convertible && measurementType === "metric") {
+        // fetching number converted from getNum()
+        [convertMe, numberIndex, size] = getNum(array);
+
+        // this is where we find measurement type, apply conversion to appropriate metric type
+        if (array.includes("cup") || array.includes("cups")) {
+            const ml = Math.floor(convertMe * 236.59)
+            const litres = convertMe * 0.236
+            if (ml >= 1000) {
+                convertedOutput[numberIndex] = litres.toString();
+                convertedOutput[numberIndex + size] = "L";
+            } else {
+                convertedOutput[numberIndex] = ml.toString();
+                convertedOutput[numberIndex + size] = "mL";
+            }
+        } else if (array.includes("teaspoon") || array.includes("teaspoons")) {
+            convertedOutput[numberIndex] = (convertMe * 4.93).toString();
+            convertedOutput[numberIndex + size] = "mL";
+        } else if (array.includes("tablespoon") || array.includes("tablespoons")) {
+            convertedOutput[numberIndex] = (convertMe * 14.79).toString();
+            convertedOutput[numberIndex + size] = "mL";
+        } else if (array.includes("pint") || array.includes("pints")) {
+            const ml = Math.floor(convertMe * 473.18)
+            const litres = convertMe * 0.473
+            if (ml >= 1000) {
+                convertedOutput[numberIndex] = litres.toString();
+                convertedOutput[numberIndex + size] = "L";
+            } else {
+                convertedOutput[numberIndex] = ml.toString();
+                convertedOutput[numberIndex + size] = "mL";
+            }
+        } else if (array.includes("quart") || array.includes("quarts")) {
+            convertedOutput[numberIndex] = (convertMe * 0.946).toString();
+            convertedOutput[numberIndex + size] = "L";
+        } else if (array.includes("gallon") || array.includes("gallons")) {
+            convertedOutput[numberIndex] = (convertMe * 3.785).toString();
+            convertedOutput[numberIndex + size] = "L";
+        } else if (array.includes("ounce") || array.includes("ounces")) {
+            convertedOutput[numberIndex] = Math.floor(convertMe * 28.35).toString();
+            convertedOutput[numberIndex + size] = "grams";
+        } else if (array.includes("pound") || array.includes("pounds")) {
+            const g = Math.floor(convertMe * 454)
+            const kg = (convertMe * 0.454)
+            if (g >= 1000) {
+                convertedOutput[numberIndex] = kg.toString();
+                convertedOutput[numberIndex + size] = "kg";
+            } else {
+                convertedOutput[numberIndex] = g.toString();
+                convertedOutput[numberIndex + size] = "grams";
+            }
+        }
+    }
+    // rounding to two decimal places
+    if (typeof convertedOutput[numberIndex] === "string" && convertedOutput[numberIndex].includes("/")) {
+        convertedOutput[numberIndex] = decimal2fraction(convertedOutput[numberIndex]);
+    } else if (typeof convertedOutput[numberIndex] === "string" && convertedOutput[numberIndex].includes(".")) {
+        convertedOutput[numberIndex] =
+            convertedOutput[numberIndex].substring(0, convertedOutput[numberIndex].indexOf(".") + 3);
+    }
+
+    // removing a context array item to accommodate for mixed fractions
+    if (size === 2) {
+        convertedOutput.splice(numberIndex + 1, 1);
+    }
+
+    return convertedOutput;
 }
 
+// returns if a string has a number
+function hasNumber(myString) {
+    return /\d/.test(myString);
+}
 
-/*
-    END OF:
-    Modifying the ingredient list
- */
+// go through all items in array, return first instance of number as float
+function getNum(array) {
+    let value = 0;
+    let size = 1;
 
+    let i = -1; // index in the array
+    let flag = true; // flag to tell if we DON'T have found the first instance of a number
 
-// The ultimate plan of this function:
+    while (flag && i <= array.length) {
+        i++;
+        if (hasNumber(array[i])) {
+            flag = false;
+            break;
+        }
+    }
 
-// 1. access the ingredient again
+    // I should have used my simplify fraction here but this does the same thing
+    if (flag) {
+        console.log("something went wrong, there should be a number in this line but there isn't");
+    } else if (array[i] && array[i + 1]) { // check for mixed fraction
+        if (Number(array[i] && array[i + 1].includes("/") && Number(array[i + 1].split("/")[0]) && Number(array[i + 1].split("/")[1]))) {
+            size = 2;
+            value = Number(array[i]) + (Number(array[i + 1].split("/")[0]) / Number(array[i + 1].split("/")[1]));
+        } else if (array[i].includes("/") && Number(array[i].split("/")[0]) && Number(array[i].split("/")[1])) {
+            size = 1;
+            value = Number(array[i].split("/")[0]) / Number(array[i].split("/")[1])
+        } else {
+            size = 1;
+            value = Number(array[i]);
+        }
+    } else if (array[i]) {
+        size = 1;
+        value = Number(array[i]);
+    }
 
-// 2. change either:
-    // 1) measurement
-    // 2) ingredient name OR
-    // 3) measurement AND measurement type
-
-
-
-function makeChanges(newIngredientList){
-    // make the changes to the actual webpage content
-
-
-
-    // let htmlIngredientList = Array.from(document.getElementsByClassName("ingredient-item"));
-
-    // This is a JQuery script, which does the following:
-    // 1. for the entire body of the webpage html (except for scripts), filter by nodeType 3 (Text only)
-    // 2. replace all instances of the current word with the word strikethrough'd and capitalized (cause y not)
-
-    finalIngredients = {"chicken":"1"}
-
-    $.each($('.ingredient-amount'), function (){
-        console.log($(this).context.innerHTML);
-        $(this).context.innerHTML = "test";
-
-
-
-    });
-
-    // console.log($('.ingredient-description').children()[0].innerHTML());
-
-    // $('body :not(script)').contents().filter(function() {
-    //     return this.nodeType === 3;
-    // }).replaceWith(function() {
-    //     return this.nodeValue.replace(wordsToChange[i],`<s>${wordsToChange[i]}</s> <b>${wordToChange.toUpperCase()}</b>`);
-    // });
-
-
+    return [value, i, size];
 }
--- practice-with-chrome-extensions/recipe/popup.js
+++ practice-with-chrome-extensions/recipe/popup.js
@@ -1,45 +1,93 @@
 
-// Add listener for "half" button.
-// If clicked, tell content-script
-let half = document.getElementById("half");
-half.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-    chrome.scripting.executeScript({
-        target: {tabId:tab.id},
-        function: sendMessageToCS(".5",tab)
-    });
+// 5/24
+// we're adding:
+// variables to track the button clicks to then send to the content_script.js
+// also:
+// removing all sendtoCS calls from button listeners that aren't the submit button
+
+let measurementClick = '';
+let metricClick = false;
+let submitClick = false;
+
+// Add listener for "half" button.
+// If clicked, tell content-script.js by calling .executeScript
+let half = document.getElementById("radio1/2");
+half.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    measurementClick = '.5';
 });
 
+// Add listener for "origianl" button.
+// If clicked, tell content-script.js by calling .executeScript
+let original = document.getElementById("radioOriginal");
+original.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
-// Add listener for "half" button.
-// If clicked, tell content-script
-let double = document.getElementById("double");
+    measurementClick = '1';
+})
+
+// Add listener for "quarter" button.
+// If clicked, tell content-script.js by calling .executeScript
+let quarter = document.getElementById("radio1/4");
+quarter.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    measurementClick = '.25';
+
+})
+
+// Add listener for "double" button.
+// If clicked, tell content-script.js by calling .executeScript
+let double = document.getElementById("radioX2");
 double.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
 
+    measurementClick = '2';
+})
+
+let convert = document.getElementById("toMetric");
+convert.addEventListener("click", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+
+    metricClick = true;
+})
+
+let submit = document.getElementById("submit_button");
+submit.addEventListener("submit", async () => {
+    let [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+
+    console.log('DEBUG submit has been clicked');
+
+    submitClick = true;
     chrome.scripting.executeScript({
-        target: {tabId:tab.id},
-        function: sendMessageToCS('2',tab)
+        target: {tabId: tab.id},
+        function: sendMessageToCS({task:'submit',measurementClicked: measurementClick, metricClicked:metricClick}, tab)
     })
 })
 
-
-/*  In order for our content-script to know what to do and when to do it,
+/*  In order for the content-script to know what to do and when to do it,
     we will do something called "message passing"
     Once the user chooses an option on the popup, we'll communicate that to the
     content-script using message passing.
     The content-script will wait until it receives that message,
     handle the message, then execute a script in response.
+
+    This is a function that FE will not need to adjust at all
 */
-
-function sendMessageToCS(message,tab) {
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tab.id, message, function(response) {});
-    });
+function sendMessageToCS(message, tab) {
+    console.log('DEBUG sending message to CS');
+    if (measurementClick !== '' && metricClick) {
+        chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+            chrome.tabs.sendMessage(tab.id, message, function (response) {
+            });
+        });
+    }
+    else{
+        console.log('cannot submit with no changes');
+    }
 
 
 }
-
-
